6ece60a3c80abba0b899eb2e707a48950c18878e
Dominic Henry <dominichenry@gmail.com>
2020-09-15 12:56:18 +0200

    Added more functionality - working with point uploads, map clearing, table outputs and plotting.

--- app.R
+++ app.R
@@ -26,37 +26,47 @@ ui <- navbarPage(title = "No-go Zones",
 
              tabPanel(title = "Interactive map",
                       leafletOutput("nogomap", width="100%", height=650),
-                      absolutePanel(id = "controls", class = "panel panel-default",
-                                    fixed = TRUE,draggable = TRUE,
+
+                      absolutePanel(id = "main_control", class = "panel panel-default",
+                                    fixed = TRUE, draggable = TRUE,
                                     top = 200, right = "auto", left = 20, bottom = "auto",
                                     width = 330, height = "auto",
 
-                                    h2("User inputs"),
+                                    # h2("User inputs"),
                                     tags$hr(),
-                                    "Shapefile uploads (add a clear feature button)",
+                                    "Shapefile uploads",
                                     fileInput("user_shape", "Choose file (csv, kml or shp)",
                                               multiple = TRUE,
                                               accept = c(".csv",".kml",".zip",
                                                          ".shx", ".shp", ".sbn", ".sbx",
                                                          ".dbf",".prj")),
                                     tags$hr(),
-                                    "Property searches (add submit or action button)",
+                                    "Property searches",
                                     textInput("sgcode", "SG CODE", ""),
-                                    br(),
                                     tags$hr(),
                                     actionButton("search_prop", "Search property"),
-                                    radioButtons("file_type", "Point or polygon",
-                                                 choices = c(Point = "point",
-                                                             Polygon = "polygon"),
-                                                 selected = "point")
+                                    # actionButton("map_reset", "Clear map"),
+                                    numericInput("lat", "Latitude", value = 0),
+                                    numericInput("long", "Longitude", value = 0),
+                                    actionButton("add_point", "Add point")
 
+                                    ),
 
-                                  )
+                      absolutePanel(id = "clear_control", class = "panel panel-default",
+                                    fixed = TRUE, draggable = FALSE,
+                                    top = 350, right = 30, left = "auto", bottom = "auto",
+                                    width = "auto", height = "auto",
+
+                                    actionButton("map_reset", "Clear map"),
+
+                                    )
                       ),
 
              tabPanel(title = "Outputs",
                       "testingtesting",
-                      tableOutput("sens_feat_table")
+                      tableOutput("sens_feat_table"),
+                      tags$hr(),
+                      tableOutput("property_table")
                       ),
 
              tabPanel(title = "Help/instructions",
@@ -65,7 +75,26 @@ ui <- navbarPage(title = "No-go Zones",
 
              )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+  ## Add user point ----
+  user_point <- reactive({
+    st_sfc(st_point(c(input$long,input$lat)),
+           crs = latlongCRS)
+  })
+
+  observeEvent(input$add_point,{
+    leafletProxy("nogomap") %>%
+      addMarkers(
+        data = user_point(),
+        popup = "User point",
+        group = "User point"
+      ) %>%
+      setView(lng = user_point()[[1]][1],
+              lat = user_point()[[1]][2],
+              zoom = 8)
+
+  })
 
   ## Plot base map ----
   output$nogomap <- renderLeaflet({
@@ -73,10 +102,29 @@ server <- function(input, output) {
   })
 
   ## Reset map to original state ----
+  observeEvent(input$map_reset,{
 
-  eventReactive(input$map_reset, {
+    leafletProxy("nogomap") %>%
+      setView(lng = 25.4015133,
+              lat = -29.1707702,
+              zoom = 6) %>%
+      clearGroup("User shapefile") %>%
+      clearGroup("User point") %>%
+      clearControls() %>%
+      addLegend("bottomright",
+                colors = c("red","lime","blue","yellow"),
+                labels = c("No-Go","PA","Farm portion","ERF"),
+                title = "Legend",
+                opacity = 0.6) %>%
+      removeLayersControl() %>%
+      addLayersControl(
+        baseGroups = c("Topographic", "Gray", "Streets","Imagery"),
+        overlayGroups= c("No-go areas","Protected areas","Farm portions","ERFs"),
+        options = layersControlOptions(collapsed=FALSE)
+      ) %>%
+      hideGroup("Farm portions")
 
-  })
+    })
 
   ##  Plot user input polygon ----
   observeEvent(input$user_shape,{
@@ -191,10 +239,8 @@ server <- function(input, output) {
 
   ## Create sensitivity data table ----
   output$sens_feat_table <- renderTable(
-
     sens_df(),
     Caption = "Test output"
-
   )
 
   ## Intersect polygon and high sensitivity layer ----
@@ -214,7 +260,42 @@ server <- function(input, output) {
 
   })
 
+  ## Create property data table ----
+  output$property_table <- renderTable(
+
+    prop_df(),
+    Caption = "Test property output"
+
+  )
+
+  ## Intersect polygon and farm layer ----
+
+  prop_df <- reactive({
+
+    req(input$user_shape)
+    farm_int <- st_intersection(user_polygon(),farms)
+
+    farm_df <- farm_int %>%
+      st_drop_geometry() %>%
+      as_tibble() %>%
+      select(-GID) %>%
+      select(PRCL_KEY, PRCL_TYPE, ID, PROVINCE, MAJ_REGION, MAJ_CODE, PARCEL_NO, PORTION) %>%
+      mutate_all(as.character)
+
+    farm_df %>%
+      mutate(prop = str_c("property_", 1:nrow(.))) %>%
+      pivot_longer(cols =-prop,
+                   names_to = "Farm_field",
+                   values_to = "value") %>%
+      pivot_wider(names_from = prop,
+                  values_from = value)
+
+  })
+
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+## Footnotes ----
+
